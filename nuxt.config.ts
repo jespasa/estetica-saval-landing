0d65cb14ca56e38895e7112f9042d039f13a6ec9
jespasa <jose.espasa.saval@gmail.com>
2025-12-20 20:14:35 +0100

    Configuración de despliegue para GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/image"],
   css: ["~/assets/css/main.css"],
+  ssr: true,
+  app: {
+    // Reemplaza 'nombre-repo' por el nombre de tu repositorio en GitHub
+    baseURL: "/estetica-saval-landing/",
+    buildAssetsDir: "assets", // Evita problemas con carpetas que empiezan por guion bajo en GitHub Pages
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true, // Rastrea todos tus links para generar las páginas estáticas
+      routes: ["/"],
+    },
+  },
+
+  // Asegúrate de que la compatibilidad con v4 esté activa para reconocer la carpeta app/
+  future: {
+    compatibilityVersion: 4,
+  },
 });
